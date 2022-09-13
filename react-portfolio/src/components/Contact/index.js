c35414363fb460593fbed197faaf48dd5e0dc413
Israel Ferreira <israel.fnds@hotmail.com>
2022-09-13 13:28:14 -0300

    att

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,34 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    return (
+        <> 
+        <div className='container contact-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['C', 'o', 'n', 't', 'a', 't', 'e', '-', 'm', 'e']} 
+                    idx={15}
+                    />
+                </h1>
+            </div>
+        </div>
+        <Loader type="pacman" />
+
+        </>
+    )
+}
+
+export default Contact
